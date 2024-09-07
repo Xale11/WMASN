359548f739fa6025eb1e17870e5c8aa6ef6412d6
Alex Diyan <alexdiyan.ad@gmail.com>
2024-09-07 21:10:51 +0100

    AWS Config file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+export default {
+  apps: [
+    {
+      name: 'WMASN',
+      script: 'server.js', // Entry point to your Node.js app
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_file: '../configWMASN/app.env', // Specify the environment file
+    },
+  ],
+};
