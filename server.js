007851cc8738f9079466dda279fabaec504111a0
Alex Diyan <alexdiyan.ad@gmail.com>
2024-09-07 22:39:11 +0100

    Fixed secret key and removed .env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express"
 import path from "path"
 import { fileURLToPath } from "url";
-import { config as dotenvConfig } from 'dotenv';
 
 
 
@@ -13,14 +12,7 @@ const __dirname = path.dirname(__filename);
 
 const buildPath = path.join(__dirname, "dist");
 
-// Local development
-dotenvConfig();
-
-// AWS deployment
-// const dotenvPath = path.resolve(__dirname, '../config4thqtr/app.env');
-// dotenvConfig({ path: dotenvPath });
-
-// app.use(express.static(buildPath))
+app.use(express.static(buildPath))
 
 
 
